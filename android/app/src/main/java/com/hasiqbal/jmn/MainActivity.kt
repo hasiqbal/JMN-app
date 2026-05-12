74fedb6066b339a1e3a781184af6f959e6ac2602
hasiqbal <ha.s.iqbal@outlook.com>
2026-05-12 17:17:52 +0100

    ,,

--- android/app/src/main/java/com/hasiqbal/jmn/MainActivity.kt
+++ android/app/src/main/java/com/hasiqbal/jmn/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hasiqbal.jmnapp
+package com.jmnapp
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/hasiqbal/jmn/MainApplication.kt
+++ android/app/src/main/java/com/hasiqbal/jmn/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.hasiqbal.jmnapp
+package com.jmnapp
 
 import android.app.Application
 import android.content.res.Configuration
